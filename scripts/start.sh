efcf8d8411fec4ef606594de313abe8ab4f25d36
Matthew Wigginton Conway <matt@indicatrix.org>
2016-05-19 16:53:59 -0400

    use java 8 on amazon linux

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Start the statistics
 
-java -Xmx1G -jar /opt/analyst-stats.jar /etc/stats.conf > /home/ec2-user/stats.log < /dev/null 2>&1 &
+java8 -Xmx1G -jar /opt/analyst-stats.jar /etc/stats.conf > /home/ec2-user/stats.log < /dev/null 2>&1 &
 echo $! > /tmp/STATS_PID
